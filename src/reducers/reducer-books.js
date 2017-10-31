a8975cf07524dd3d5cc881682fe23bcebc9b6dba
Puls <cg3t@M-C02F2DDFD59.nordstrom.net>
2017-10-31 11:53:43 -0700

    redux part 2

---
+++ src/reducers/reducer-books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    {title: 'Javascript: The Good Parts', pages: 300},
+    {title: 'Harry Potter', pages: 200},
+    {title: 'The Dark Tower', pages: 400},
+    {title: 'Eloquent Javascript', pages: 500}
+  ]
+}
